d8648842c8759cd803f3eef2b12be169b68b89c0
Dave Haeffner <dhaeffner@gmail.com>
2016-06-25 09:41:54 +0530

    Added screenshot test back in (fixing dependency issue)

---
+++ src/test/java/ScreenshotTest.java
@@ -0,0 +1,40 @@
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import java.io.File;
+import java.io.IOException;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ScreenshotTest extends BaseTest {
+    WebDriver driver;
+
+    @Rule
+    public TestRule watcher = new TestWatcher() {
+        @Override
+        protected void failed(Throwable throwable, Description description) {
+            File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            try {
+                org.apache.commons.io.FileUtils.copyFile(scrFile,
+                        new File("failshot_"
+                                + description.getClassName()
+                                + "_" + description.getMethodName()
+                                + ".png"));
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+    };
+
+    @Test
+    public void OnError() {
+        driver.get("http://the-internet.herokuapp.com");
+        assertThat(false, is(true));
+    }
+
+}
